3d8dbda6d8b60f4b376946f0be884e58ca38ab6b
T00037342 <T00037342@utbvirtual.edu.co>
2015-03-04 16:53:37 -0500

    parcial

--- AppServicioEntregaDePaquetesAsp.Net/Modelo/TwoDayPackage.cs
+++ AppServicioEntregaDePaquetesAsp.Net/Modelo/TwoDayPackage.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace AppServicioEntregaDePaquetesAsp.Net.Datos
+namespace AppServicioEntregaDePaquetesAsp.Net.Modelo
 {
     public class TwoDayPackage : Package
     {
